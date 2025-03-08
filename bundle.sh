1336aabceb93b9dc299d1fb4d2b0572b15736feb
mardhiluthfilhadi <mardhiluthfilhadi@yahoo.com>
2025-03-08 14:54:55 +0700

    update bundler

--- bundle.sh
+++ bundle.sh
@@ -6,9 +6,12 @@ fi;
 
 if [ ! -d build/linux64 ]; then
     mkdir build/linux64
+    mkdir build/linux64/libs
 fi
 
 gcc -shared -fPIC -o ./libs/libtest.so ./src/test.c -lm
-zip -9 ./build/pong-bang.love -r ./assets/ ./libs/libtest.so ./src main.lua
+zip -9 ./build/pong-bang.love -r ./assets/ ./src main.lua
 cat ./libs/linux64/love ./build/pong-bang.love > ./build/linux64/pong-bang
+cp ./libs/libtest.so ./build/linux64/libs/
 chmod +x ./build/linux64/pong-bang
+cd build/linux64/ && zip -9 pong-bang-linux-x64.zip ./pong-bang libs/libtest.so
--- bundle_mingw.sh
+++ bundle_mingw.sh
@@ -7,9 +7,12 @@ fi;
 
 if [ ! -d build/win64 ]; then
     mkdir build/win64
+    mkdir build/win64/libs
 fi
 
 x86_64-w64-mingw32-gcc -shared -fPIC -o ./libs/libtest.so ./src/test.c -lm
-zip -9 ./build/pong-bang.love -r ./assets/ ./libs/libtest.so ./src main.lua
+zip -9 ./build/pong-bang.love -r ./assets/ ./src main.lua
 cat ./libs/win64/love.exe ./build/pong-bang.love > ./build/win64/pong-bang.exe
+cp ./libs/libtest.so ./build/win64/libs/
 cp ./libs/win64/*.dll ./build/win64
+cd build/win64 && zip -9 pong-bang-win-x64.zip pong-bang.exe libs/libtest.so *.dll
